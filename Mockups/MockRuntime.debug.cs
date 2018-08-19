25a8db40851f2de29d69fd64862863e02a317fff
bls220 <bls220@vt.edu>
2018-08-19 18:14:10 -0400

    Initial Pass at having a UI

--- Mockups/MockRuntime.debug.cs
+++ Mockups/MockRuntime.debug.cs
@@ -18,5 +18,16 @@ namespace IngameScript.Mockups
         public virtual int CurrentMethodCallCount { get; set; }
 
         public virtual UpdateFrequency UpdateFrequency { get; set; }
+
+        public int MaxCallChainDepth
+        {
+            get
+            {
+                // TODO: check in SE to get this value.
+                return 10;
+            }
+        }
+
+        public int CurrentCallChainDepth { get; } 
     }
 }
---
+++ Mockups/UIMockedRun.debug.cs
@@ -0,0 +1,37 @@
+﻿using IngameScript.Mockups;
+using System;
+using System.Windows.Threading;
+
+namespace IngameScript.UI
+{
+    public class UIMockedRun : MockedRun
+    {
+        // Possibly placeholder until the Presentation Layer is made.
+        private MainWindow Ui { get; set; }
+
+        public UIMockedRun(MainWindow ui)
+        {
+            this.Ui = ui;
+        }
+
+        bool firstEcho = true;
+        public override void Echo(string text)
+        {
+            if (firstEcho)
+            {
+                firstEcho = false;
+                Ui.EchoBlock.Dispatcher.BeginInvoke(DispatcherPriority.DataBind, new Action(() => Ui.EchoBlock.Text = $"{text}\n"));
+            }
+            else
+            {
+                Ui.EchoBlock.Dispatcher.BeginInvoke(DispatcherPriority.DataBind, new Action(() => Ui.EchoBlock.Text += $"{text}\n"));
+            }
+        }
+
+        public override bool NextTick(out MockedRunFrame frame)
+        {
+            firstEcho = true;
+            return base.NextTick(out frame);
+        }
+    }
+}
